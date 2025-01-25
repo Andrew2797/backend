5939c03d5b6b0601b84f3a57cf306891a45c9b1e
Andrew2797 <andrijgolub27@gmail.com>
2025-01-25 09:32:46 +0200

    wait

--- src/database/db_actions.py
+++ src/database/db_actions.py
@@ -5,11 +5,13 @@ from src.database.models import db, Product, Review
 
 
 def get_products() -> List[Product]:
+    #return db.get_or_404(db.session.query(Product))
     return db.session.query(Product).all()
 
 
 def get_product(prod_id: str) -> Product:
-    return db.session.query(Product).where(Product.id == prod_id).first()
+    db.one_or_404(db.session.query(Product).where(Product.id == prod_id))
+    #return db.session.query(Product).where(Product.id == prod_id).first()
 
 
 def add_product(
@@ -37,7 +39,7 @@ def edit_product(
         img_url: str,
         price: float
 ) -> str:
-    product = db.session.query(Product).where(Product,id == prod_id).first()
+    product = db.one_or_404(db.session.query(Product).where(Product,id == prod_id))
     product.name = name 
     product.description = description
     product.img_url = img_url
@@ -47,15 +49,38 @@ def edit_product(
 
 
 def del_product(prod_id: str) -> str:
-    product = db.session.query(Product).where(Product.id == prod_id).first()
+    product = db.one_or_404(db.session.query(Product).where(Product.id == prod_id))
     db.session.delete(product)
     db.session.commit()
     return "Successful"
 
 
 def add_review_by_product(reviwe: Review, prod_id: id) -> str:
-    product = db.session.query(Product).where(Product.id == prod_id).first()
+    product = db.one_or_404(db.session.query(Product).where(Product.id == prod_id))
     product.reviews.append(product)
     db.session.commit()
     return "Successful" 
+
+
+def get_reviews() -> List[Review]:
+    return db.session.query(Review).all()
+
+
+def get_review(review_id: str) -> Review:
+    return db.one_or_404(db.session.query(Review).where(Review.id == review_id))
+
+
+def edit_review(review_id: str, content: str, author: str) -> str:
+    review = db.one_or_404(db.session.query(Review).where(Review.id == review_id))
+    review.content = content
+    review.author = author
+    db.session.commit()
+    return "Successful"
+
+
+def del_review(review_id: str) -> str:
+    review = db.one_or_404(db.session.query(Review).where(Review.id == review_id))
+    db.session.delete(review)
+    db.session.commit()
+    return "Successful"
 
